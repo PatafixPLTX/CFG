23e9ec1dbb4111b0a4eaaefdba7cbbbc5be74ea1
YLUX-project <YLUXos.systeme@gmail.com>
2022-03-06 10:31:11 +0100

    add basic systeme of vue.js

---
+++ assets/js/vue.js
@@ -0,0 +1,39 @@
+
+
+
+const Play = {
+    template: '<h1>Play Vue</h1>',
+    name: Play
+}
+
+const Home = {
+    template: '<h1>Home Vue</h1>',
+    name: Home
+}
+
+const News = {
+    template: '<h1>News Vue</h1>',
+    name: News
+}
+
+const Shop = {
+    template: '<h1>Shop Vue</h1>',
+    name: Shop
+}
+
+const Rank = {
+    template: '<h1>Rank Vue</h1>',
+    name: Rank
+}
+
+const Settings = {
+    template: '<h1>Settings Vue</h1>',
+    name: Settings
+}
+
+
+
+
+const vue = new vue({
+    router
+}).$mount('#app');
