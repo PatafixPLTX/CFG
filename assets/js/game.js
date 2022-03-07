0a69ab9d68b3b8b63e2c757de766946bfcca5629
Bot-maker <72702161+BOT-maKeR-0000@users.noreply.github.com>
2022-03-07 19:00:58 +0100

    second stroke

--- assets/js/game.js
+++ assets/js/game.js
@@ -41,6 +41,11 @@ function drawEnemyCircle(posX, posY) {
     ctx.fillStyle = "red";
     ctx.arc(posX * oneOnTenWidth + oneOnTenWidth / 2, posY * oneOnTenHeigth + oneOnTenHeigth / 2, oneOnTenHeigth / 2 - 4, 0, 2 * Math.PI, false);
     ctx.fill();
+    ctx.beginPath();
+    ctx.strokeStyle = "white";
+    ctx.moveTo(posX*oneOnTenWidth+4, posY * oneOnTenHeigth + oneOnTenHeigth / 2);
+    ctx.lineTo(posX * oneOnTenWidth + oneOnTenWidth-4, posY * oneOnTenHeigth + oneOnTenHeigth / 2);
+    ctx.stroke();
 }
 
 $(document).ready(function () {
