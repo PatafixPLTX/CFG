b0ba63d055d3ca2d1161872f0f6a924a7a33e57c
Bot-maker <72702161+BOT-maKeR-0000@users.noreply.github.com>
2022-03-08 19:47:44 +0100

    play.js

--- assets/page/play.js
+++ assets/page/play.js
@@ -1,1 +1,4 @@
-content.innerHTML = '<script src="js/game.js"></script>';
+content.innerHTML = "<h1>The game</h1>";
+let canvasScript = document.createElement("script");
+canvasScript.src = "js/game.js";
+content.appendChild(canvasScript);
