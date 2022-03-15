2c0acfd7261c21cc00f0e6e552bd51a987376a6a
Bot-maker <72702161+BOT-maKeR-0000@users.noreply.github.com>
2022-03-15 17:02:24 +0100

    test slider + css

--- assets/page/settings.js
+++ assets/page/settings.js
@@ -1,4 +1,6 @@
 content.innerHTML = '<h1>Settings</h1><div class="slidecontainer"><input type="range" min="1" max="100" value="50" class="slider" id="myRange"></div>';
-contentCreation = document.createElement("style");
-contentCreation.src = "style/sliderbarStyle.css";
+contentCreation = document.createElement("link");
+contentCreation.rel = "stylesheet";
+contentCreation.type = "text/css";
+contentCreation.href = "style/sliderStyle.css";
 content.appendChild(contentCreation);
