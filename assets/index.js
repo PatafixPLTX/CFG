97434fbab7da3edcc2351e17ce88b54619c61b8a
Bot-maker <72702161+BOT-maKeR-0000@users.noreply.github.com>
2022-03-16 19:29:58 +0100

    changed nodejs port

--- assets/index.js
+++ assets/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const fs = require('fs');
 const app = express();
-const port = 333;
+const port = 80;
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
