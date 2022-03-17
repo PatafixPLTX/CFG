172e30b96771d1dadb3e7428dc54f7c480e8a1d4
Bot-maker <72702161+BOT-maKeR-0000@users.noreply.github.com>
2022-03-17 18:51:43 +0100

    test

---
+++ assets/js/home.js
@@ -0,0 +1,25 @@
+function calcCanvasSize(windowWidth, windowHeight){
+    if(windowHeight>windowWidth) this.size = windowWidth;
+    else                         this.size = windowHeight;
+    this.size *= preferredPercentageSize;
+    if(this.size>preferenceMaxCanvasSize) this.size = preferenceMaxCanvasSize;
+    if(this.size<preferenceMinCanvasSize) this.size = preferenceMinCanvasSize;
+    this.size -= this.size%10;
+    return this.size;
+};
+
+$(document).ready(function () {
+    canvas = document.createElement('canvas');
+    canvas.width = calcCanvasSize($(window).width()-250, $(window).height());    
+    canvas.height = canvas.width;
+    canvas.style = "z-index: 10;background: transparent;padding: 0;margin: auto;display: block;position: absolute;left:78px;right:0px;bottom:0px;top: 0px;border: 5px black solid;transition: all 0.5s ease;";
+    document.body.appendChild(canvas);
+    halfWidthCanvas = canvas.width / 2;
+    halfHeigthCanvas = canvas.height / 2;
+
+    ctx = canvas.getContext("2d");
+
+    ctx.beginPath();
+    ctx.fillColor = "rgb(255,0,0);";
+    ctx.rect(25,25,125,365);
+});
