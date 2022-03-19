7a145e1b6390bca9ff5f6f911f211944f269f070
Patafix <project.patafix@gmail.com>
2022-03-19 15:30:29 +0100

    home.js

--- assets/page/home.js
+++ assets/page/home.js
@@ -1,1 +1,12 @@
-content.innerHTML = "<h1>Hello World!!</h1>"
+content.innerHTML = ```<div class="title">
+<!-- 80% -->
+
+<h1>Connect Four Gravity</h1>
+
+</div>
+<div class="text">
+<!-- 95% -->
+
+<p>Je suis un texte...</p>
+
+</div>```
